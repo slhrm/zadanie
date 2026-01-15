df2c92a060f2ac2c03376e8849759f7df5b7cf16
slhrm <srmumun24a@gmail.com>
2026-01-15 10:29:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,30 @@ st.title("Любими неща - класна анкета")
 
 # Инициализация на данните
 if "colors" not in st.session_state:
-    st.session_state.colors = {
-        "Червен": 0,
-        "Син": 0,
-        "Зелен": 0,
-        "Жълт": 0
+    st.session_state.grades = {
+        "Слаб (2)": 0,
+        "Среден (3)": 0,
+        "Добър (4)": 0,
+        "Мн. Добър (5)": 0,
+        "Отличен (6)": 0
     }
 
 if "sports" not in st.session_state:
-    st.session_state.sports = {
-        "Футбол": 0,
-        "Баскетбол": 0,
-        "Волейбол": 0,
-        "Плуване": 0
+    st.session_state.students = {
+        "Salih": 0,
+        "Rado": 0,
+        "Nasko": 0,
+        "Mitko": 0
     }
 
 st.subheader("Избери любими неща")
 
-color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
-sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
+grades = st.selectbox("Оценка:", list(st.session_state.colors.keys()))
+students = st.selectbox("Ученик:", list(st.session_state.sports.keys()))
 
 if st.button("Запази избора"):
-    st.session_state.colors[color] += 1
-    st.session_state.sports[sport] += 1
+    st.session_state.grades[grades] += 1
+    st.session_state.students[students] += 1
     st.success("Изборът е записан!")
 
 st.divider()
@@ -36,14 +37,14 @@ st.subheader("Резултати")
 
 
 st.write("Любими цветове")
-colors_df = pd.DataFrame.from_dict(
-    st.session_state.colors, orient="index", columns=["Брой"]
+grades_df = pd.DataFrame.from_dict(
+    st.session_state.grades, orient="index", columns=["Брой"]
 )
-st.bar_chart(colors_df)
+st.bar_chart(grades_df)
 
 
 st.write("Любими спортове")
-sports_df = pd.DataFrame.from_dict(
-    st.session_state.sports, orient="index", columns=["Брой"]
+students_df = pd.DataFrame.from_dict(
+    st.session_state.students, orient="index", columns=["Брой"]
 )
-st.bar_chart(sports_df)
+st.bar_chart(students_df)
