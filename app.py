24f0e20085249d9bff71fa0c0ba721b397a4328e
slhrm <srmumun24a@gmail.com>
2026-01-15 10:17:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+import streamlit as st
+import pandas as pd
 
+st.title("Любими неща - класна анкета")
+
+# Инициализация на данните
+if "colors" not in st.session_state:
+    st.session_state.colors = {
+        "Червен": 0,
+        "Син": 0,
+        "Зелен": 0,
+        "Жълт": 0
+    }
+
+if "sports" not in st.session_state:
+    st.session_state.sports = {
+        "Футбол": 0,
+        "Баскетбол": 0,
+        "Волейбол": 0,
+        "Плуване": 0
+    }
+
+st.subheader("Избери любими неща")
+
+color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
+sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
