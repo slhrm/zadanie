1ad0bb1893d5bb0b4a7a46032726c1d9fcfce876
slhrm <srmumun24a@gmail.com>
2026-01-15 10:23:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,3 +24,26 @@ st.subheader("Избери любими неща")
 
 color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
 sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
+
+if st.button("Запази избора"):
+    st.session_state.colors[color] += 1
+    st.session_state.sports[sport] += 1
+    st.success("Изборът е записан!")
+
+st.divider()
+
+st.subheader("Резултати")
+
+
+st.write("Любими цветове")
+colors_df = pd.DataFrame.from_dict(
+    st.session_state.colors, orient="index", columns=["Брой"]
+)
+st.bar_chart(colors_df)
+
+
+st.write("Любими спортове")
+sports_df = pd.DataFrame.from_dict(
+    st.session_state.sports, orient="index", columns=["Брой"]
+)
+st.bar_chart(sports_df)
